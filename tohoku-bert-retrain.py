65a855d30e2903f1c91342ae7bbc8a0f3680f7e5
ToshihikoSakai <s73041h20@yahoo.co.jp>
2022-06-11 15:44:31 +0900

    formatterの対応とDataCollatorForWholeWordMaskへの変更及びマスクされた単語の確認の処理の追加

--- tohoku-bert-retrain.py
+++ tohoku-bert-retrain.py
@@ -1,4 +1,4 @@
-from transformers import BertConfig
+from transformers import BertConfig, DataCollatorForWholeWordMask
 from transformers import BertJapaneseTokenizer, BertModel
 from transformers import BertForMaskedLM
 from transformers import LineByLineTextDataset
@@ -11,6 +11,8 @@ import os
 import torch
 import time
 import sys
+from torch.utils.data import DataLoader
+
 
 def main():
     start = time.time()
@@ -21,59 +23,77 @@ def main():
     device = torch.device("cuda:0" if cuda_yes else "cpu")
     print('Device:', device)
 
-
     dir = os.getcwd()
 
     corpus = args[1]
     outputdir = args[2]
 
-
     # 事前学習用コーパスの準備
     # 1行に1文章となるようなテキストを準備する
-    #df_header = pd.read_csv('XXX.csv')
-    #print(df_header)
+    # df_header = pd.read_csv('XXX.csv')
+    # print(df_header)
 
     # 東北大学BERTのtokenizer(mecab+wordpiece)を使う
-    tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+    tokenizer = BertJapaneseTokenizer.from_pretrained(
+        'cl-tohoku/bert-base-japanese-whole-word-masking')
 
     print(tokenizer)
     # textをトークナイズ
-    #text = ""
-    #print(tokenizer.tokenize(text))
+    # text = ""
+    # print(tokenizer.tokenize(text))
 
     # BERTモデル
     # BERT MLMのインスタンスを生成
-    model = BertForMaskedLM.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+    model = BertForMaskedLM.from_pretrained(
+        'cl-tohoku/bert-base-japanese-whole-word-masking')
     model.to(device)
 
     print(model)
 
     # パラメータ数を表示
     print('No of parameters: ', model.num_parameters())
-    
+
     # textを1行ずつ読み込んでトークンへ変換
     dataset = LineByLineTextDataset(
-         tokenizer=tokenizer,
-         file_path=corpus,
-         block_size=512, # tokenizerのmax_length
-         # block_sizeはtokenizerのmax_lengthっぽい
-         #https://github.com/huggingface/transformers/blob/master/src/transformers/data/datasets/language_modeling.py#L114
-
-
+        tokenizer=tokenizer,
+        file_path=corpus,
+        block_size=512,  # tokenizerのmax_length
+        # block_sizeはtokenizerのmax_lengthっぽい
+        # https://github.com/huggingface/transformers/blob/master/src/transformers/data/datasets/language_modeling.py#L114
     )
 
     print('No. of lines: ', len(dataset))
 
     # データセットからサンプルのリストを受け取り、それらをテンソルの辞書としてバッチに照合するための関数
-    data_collator = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer, 
+    data_collator = DataCollatorForWholeWordMask(
+        tokenizer=tokenizer,
         mlm=True,
-        mlm_probability= 0.15
+        mlm_probability=0.15
     )
+
+    # 単語マスクの状況確認のためDataLoaderを作成
+    dataloader = DataLoader(
+        dataset,
+        shuffle=False,
+        batch_size=2,
+        collate_fn=data_collator
+    )
+
+    # tokenizerの状況確認
+    for i, sen in enumerate(dataset):
+        print(tokenizer.convert_ids_to_tokens(sen['input_ids']))
+        if(i == 10):
+            break
+
+    # 単語マスクの状況確認
+    for i, batch in enumerate(dataloader):
+        print(tokenizer.decode(batch['input_ids'][1]))
+        if(i == 10):
+            break
 
     # 事前学習のパラメータを定義
     training_args = TrainingArguments(
-        output_dir= outputdir,
+        output_dir=outputdir,
         overwrite_output_dir=True,
         num_train_epochs=10,
         per_device_train_batch_size=32,
@@ -97,12 +117,11 @@ def main():
     # 学習したモデルの保存
     trainer.save_model(outputdir)
 
-    #かかった時間を出力
+    # かかった時間を出力
     elapsed_time = time.time() - start
     print("elapsed_time={}".format(elapsed_time))
-    with open('./elapsed_time.txt',mode='w') as f:
+    with open('./elapsed_time.txt', mode='w') as f:
         f.write("elapsed_time={}".format(elapsed_time))
-
 
 
 if __name__ == '__main__':
